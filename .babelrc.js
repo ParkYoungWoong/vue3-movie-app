dd7ff16775893f10236fdb8e6581e19f443c826d
ParkYoungWoong <thesecon@gmail.com>
2021-04-20 19:15:17 +0900

    Fix the bug that error message is displayed as error object

--- .babelrc.js
+++ .babelrc.js
@@ -1,7 +1,13 @@
 module.exports = {
   presets: ['@babel/preset-env'],
   plugins: [
-    ['@babel/plugin-transform-runtime'],
-    ['transform-remove-console']
-  ]
+    ['@babel/plugin-transform-runtime']
+  ],
+  env: {
+    production: {
+      plugins: [
+        'transform-remove-console'
+      ]
+    }
+  }
 }
