b0b55093fb89fef5475daa742707d5ae8d98a7b8
ParkYoungWoong <thesecon@gmail.com>
2021-04-20 19:42:10 +0900

    Modify the transform-remove-console(babel) to work only in production mode

--- .babelrc.js
+++ .babelrc.js
@@ -1,13 +1,12 @@
+const isProd = process.env.NODE_ENV === 'production'
+const plugins = isProd
+  ? [['@babel/plugin-transform-runtime']]
+  : [
+    ['@babel/plugin-transform-runtime'],
+    ['transform-remove-console']
+  ]
+
 module.exports = {
   presets: ['@babel/preset-env'],
-  plugins: [
-    ['@babel/plugin-transform-runtime']
-  ],
-  env: {
-    production: {
-      plugins: [
-        'transform-remove-console'
-      ]
-    }
-  }
+  plugins
 }
