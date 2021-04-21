c528c975b97505d00bb6d368579a9f3fe4348a09
ParkYoungWoong <thesecon@gmail.com>
2021-04-21 13:23:15 +0900

    Remove babel remove console

--- .babelrc.js
+++ .babelrc.js
@@ -1,12 +1,6 @@
-const isProd = process.env.NODE_ENV === 'production'
-const plugins = isProd
-  ? [['@babel/plugin-transform-runtime']]
-  : [
-    ['@babel/plugin-transform-runtime'],
-    ['transform-remove-console']
-  ]
-
 module.exports = {
   presets: ['@babel/preset-env'],
-  plugins
+  plugins: [
+    ['@babel/plugin-transform-runtime']
+  ]
 }
