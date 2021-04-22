0fd57d90744154e055464b1545610e07efbb34c6
ParkYoungWoong <thesecon@gmail.com>
2021-04-22 14:37:10 +0900

    Add comments

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,24 @@
 module.exports = {
-  // 사전에 정의된 전역 변수 설정
+  // 사전에 정의된 전역 변수를 설정합니다.
   env: {
     browser: true,
     node: true
   },
-  // 사용 가능한 규칙 세트
+
+  // 사용 가능한 규칙 세트입니다.
   extends: [
-    // 'plugin:vue/vue3-essential', // Lv1
-    'plugin:vue/vue3-strongly-recommended', // Lv2
-    // 'plugin:vue/vue3-recommended', // Lv3
+    // 'plugin:vue/vue3-essential', // Vue Lv1
+    'plugin:vue/vue3-strongly-recommended', // Vue Lv2
+    // 'plugin:vue/vue3-recommended', // Vue Lv3
     'eslint:recommended'
   ],
-  // 구문 분석할 패키지 설정(Babel, ES6^ => ES5)
+
+  // 구문 분석할 패키지를 설정합니다.(Babel, ES6^ => ES5)
   parserOptions: {
     parser: 'babel-eslint'
   },
-  // 추가적인 코드 규칙을 설정
+
+  // 추가적인 코드 규칙을 설정합니다.
   rules: {
     'vue/html-self-closing': ['error', {
       html: {
@@ -31,6 +34,9 @@ module.exports = {
       multiline: 'never'
     }]
   },
+
+  // 패턴에 일치하는 파일에는 jest 문법을 재정의합니다.
+  // 테스트를 위한 파일을 지정합니다.
   overrides: [
     {
       files: [
--- jest.config.js
+++ jest.config.js
@@ -6,17 +6,22 @@ module.exports = {
     'json',
     'vue'
   ],
+
   // `~` 같은 경로 별칭을 매핑합니다.
   // E.g. `import HelloWorld from '~/components/HelloWorld.vue';`
   moduleNameMapper: {
     '^~/(.*)$': '<rootDir>/src/$1'
   },
+
   // 테스트 환경을 지정합니다.
-  // jsdom의 최신 버전을 별도 사용하는 경우에는, 
+  // jsdom의 최신 버전을 별도 사용하는 경우에는,
   // 다음과 같이 해당 모듈을 설치 후 옵션을 지정합니다.
   testEnvironment: 'jest-environment-jsdom-sixteen',
+
+  // jsdom 환경에 대한 URL을 설정합니다.
   // https://github.com/facebook/jest/issues/6766
   testURL: 'http://localhost/',
+
   // 정규식과 일치하는 파일의 변환 모듈을 지정합니다.
   transform: {
     '^.+\\.vue$': 'vue-jest',
